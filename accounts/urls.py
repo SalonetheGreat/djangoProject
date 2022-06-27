b6795d716faa9faa0efb0c8e4b2e2d3023402354
SalonetheGreat <shalong5794@gmail.com>
2022-06-27 13:29:02 +0800

    create html templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,38 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'accounts/index.html', {})
+    if request.method == 'GET':
+        return render(request, 'accounts/index.html')
+    elif request.method == 'POST':
+        return HttpResponse('We have received a POST request @index')
+    else:
+        raise Http404()
 
 
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html')
+    elif request.method == 'POST':
+        return HttpResponse('We have received a POST request @login')
+    else:
+        raise Http404()
+
+
+def logout(request):
+    if request.method == 'POST':
+        return HttpResponse('We have received a POST request @logout')
+    else:
+        raise Http404()
+
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'accounts/register.html')
+    elif request.method == 'POST':
+        return HttpResponse('We have received a POST request @register')
+    else:
+        raise Http404()
 
 
